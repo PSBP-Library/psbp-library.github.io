cc0f5dd9fa94a9147b012da0a50f423a3201a196
Luc Duponcheel <luc.duponcheel@gmail.com>
2021-02-24 14:07:22 +0100

    avoiding horizontal scrolling in code of blog

--- src/main/scala/examples/specification/program/Condition.scala
+++ src/main/scala/examples/specification/program/Condition.scala
@@ -2,10 +2,12 @@ package examples.specification.program
 
 import psbp.specification.program.{ ||, Program, `(z||z)>-->z`, `(y||x)>-->b`, `(y||x)>-->y`, `(y||x)>-->x` }
 
-def `conditionally using |||`[>-->[- _, + _]: Program, Z, Y, X](`y>-->z`: => Y >--> Z, `x>-->z`: => X >--> Z): (Y || X) >--> Z =
+def `conditionally using |||`[>-->[- _, + _]: Program, Z, Y, X]
+  (`y>-->z`: => Y >--> Z, `x>-->z`: => X >--> Z): (Y || X) >--> Z =
   (`y>-->z` ||| `x>-->z`) >--> `(z||z)>-->z`
 
-def conditionallyUsingIf[>-->[- _, + _]: Program, Z, Y, X](`y>-->z`: => Y >--> Z, `x>-->z`: => X >--> Z): (Y || X) >--> Z =
+def conditionallyUsingIf[>-->[- _, + _]: Program, Z, Y, X]
+  (`y>-->z`: => Y >--> Z, `x>-->z`: => X >--> Z): (Y || X) >--> Z =
 
   val program: Program[>-->] = summon[Program[>-->]]
   import program.If
--- src/main/scala/examples/specification/program/Construction.scala
+++ src/main/scala/examples/specification/program/Construction.scala
@@ -2,10 +2,12 @@ package examples.specification.program
 
 import psbp.specification.program.{ &&, Program, `z>-->(z&&z)`, `(z&&y)>-->z` , `(z&&y&&x)>-->(y&&x)` }
 
-def `construct using &&&`[>-->[- _, + _]: Program, Z, Y, X] (`z>-->y`: Z >--> Y, `z>-->x`: => Z >--> X): Z >--> (Y && X) =
+def `construct using &&&`[>-->[- _, + _]: Program, Z, Y, X] 
+  (`z>-->y`: Z >--> Y, `z>-->x`: => Z >--> X): Z >--> (Y && X) =
   `z>-->(z&&z)` >--> (`z>-->y` &&& `z>-->x`)
 
-def constructUsingLet[>-->[- _, + _]: Program, Z, Y, X] (`z>-->y`: Z >--> Y, `z>-->x`: => Z >--> X): Z >--> (Y && X) =
+def constructUsingLet[>-->[- _, + _]: Program, Z, Y, X] 
+  (`z>-->y`: Z >--> Y, `z>-->x`: => Z >--> X): Z >--> (Y && X) =
  
   val program: Program[>-->] = summon[Program[>-->]]
   import program.Let
